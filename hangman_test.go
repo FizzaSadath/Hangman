ffe52a9acb5fbf6f2a9a0c96a7c9feda56c207ce
Fizza Sadath <fizzasadath@gmail.com>
2025-10-15 15:23:44 +0530

    RED: Adds test case for checking word has no punctuation

--- hangman_test.go
+++ hangman_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strings"
 	"testing"
+	"unicode"
 )
 
 func TestSecretWordNoCapitals(t *testing.T) {
@@ -12,4 +13,19 @@ func TestSecretWordNoCapitals(t *testing.T) {
 		t.Errorf("Should not get words with capital letters. Got %s", secretWord)
 	}
 
+}
+func isAllLetters(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+func TestSecretWordNoPunc(t *testing.T) {
+	wordList := "/usr/share/dict/words"
+	secretWord := getSecretWord(wordList)
+	if !isAllLetters(secretWord) {
+		t.Errorf("Should not get words with punctuations. Got %s", secretWord)
+	}
 }
