0fd0f288704b8790dd19181f4e7d52e7afcd232f
Fizza Sadath <fizzasadath@gmail.com>
2025-10-15 16:28:09 +0530

    RED: Adds test case for checking minimum length is six.

--- hangman_test.go
+++ hangman_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"strings"
 	"testing"
-	"unicode"
 )
 
 func TestSecretWordNoCapitals(t *testing.T) {
@@ -14,18 +13,19 @@ func TestSecretWordNoCapitals(t *testing.T) {
 	}
 
 }
-func isAllLetters(s string) bool {
-	for _, r := range s {
-		if !unicode.IsLetter(r) {
-			return false
-		}
-	}
-	return true
-}
+
 func TestSecretWordNoPunc(t *testing.T) {
 	wordList := "/usr/share/dict/words"
 	secretWord := getSecretWord(wordList)
 	if !isAllLetters(secretWord) {
 		t.Errorf("Should not get words with punctuations. Got %s", secretWord)
+	}
+}
+
+func TestSecretWordLength(t *testing.T) {
+	wordList := "/usr/share/dict/words"
+	secretWord := getSecretWord(wordList)
+	if len(secretWord) < 6 {
+		t.Errorf("Minimum word length is six. Got %s", secretWord)
 	}
 }
