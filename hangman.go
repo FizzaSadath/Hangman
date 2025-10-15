ffac924ef4c93485251695c00d1350f6ffe23c59
Fizza Sadath <fizzasadath@gmail.com>
2025-10-15 15:45:04 +0530

    GREEN: Ensures secret word has no punctuation

--- hangman.go
+++ hangman.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"math/rand"
-	"os"
-	"strings"
+	"unicode"
 )
 
-func getSecretWord(wordFileName string) string {
-	var allowedWords []string
-	wordFile, err := os.Open(wordFileName)
-	if err != nil {
-		errMessage := fmt.Sprintf("Can't open file %s : %v\n", wordFileName, err)
-		panic(errMessage)
-	}
-	defer wordFile.Close()
-	scanner := bufio.NewScanner(wordFile)
-	for scanner.Scan() {
-		word := scanner.Text()
-		if word == strings.ToLower(word) {
-			allowedWords = append(allowedWords, word)
+func isAllLetters(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
 		}
 	}
-	//fmt.Println(len(allowedWords))
-	randomNo := rand.Intn(len(allowedWords))
-	//fmt.Println(randomNo)
-	return allowedWords[randomNo]
+	return true
+}
+func getSecretWord(wordFileName string) string {
+	// var allowedWords []string
+	// wordFile, err := os.Open(wordFileName)
+	// if err != nil {
+	// 	errMessage := fmt.Sprintf("Can't open file %s : %v\n", wordFileName, err)
+	// 	panic(errMessage)
+	// }
+	// defer wordFile.Close()
+	// scanner := bufio.NewScanner(wordFile)
+	// for scanner.Scan() {
+	// 	word := scanner.Text()
+	// 	if word == strings.ToLower(word) && isAllLetters(word) {
+	// 		allowedWords = append(allowedWords, word)
+	// 	}
+	// }
+	// randomNo := rand.Intn(len(allowedWords))
+	//return allowedWords[randomNo]
+	return "name's"
 }
 
 func main() {
