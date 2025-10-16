0a962a58471d890001559c09bad56813159260ff
Fizza Sadath <fizzasadath@gmail.com>
2025-10-16 16:04:06 +0530

    Makes the test cases of getSecretWord deterministic.

--- hangman_test.go
+++ hangman_test.go
@@ -1,31 +1,76 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
 
-func TestSecretWordNoCapitals(t *testing.T) {
-	wordList := "/usr/share/dict/words"
-	secretWord := getSecretWord(wordList)
-	if secretWord != strings.ToLower(secretWord) {
-		t.Errorf("Should not get words with capital letters. Got %s", secretWord)
+func createDictFile(words []string) (string, error) {
+	f, err := os.CreateTemp("/tmp", "hangman-dict")
+	if err != nil {
+		fmt.Println("Couldn't create temp file.")
 	}
+	data := strings.Join(words, "\n")
+	_, err = f.Write([]byte(data))
+	if err != nil {
+		return "", err
+	}
+	return f.Name(), nil
+}
+
+func TestSecretWordNoCapitals(t *testing.T) {
+	wordList, err := createDictFile([]string{"Lion", "Elephant", "monkey"})
+	defer os.Remove(wordList)
+	if err != nil {
+
+		t.Errorf("Couldn't create word list. Can't proceed with test : %v", err)
+	}
+	secretWord := getSecretWord(wordList)
+	if secretWord != "monkey" {
+		t.Errorf("Should get 'monkey' but Got %s", secretWord)
+	}
+	//wordList := "/usr/share/dict/words"
+	// secretWord := getSecretWord(wordList)
+	// if secretWord != strings.ToLower(secretWord) {
+	// 	t.Errorf("Should not get words with capital letters. Got %s", secretWord)
+	// }
 
 }
 
 func TestSecretWordNoPunc(t *testing.T) {
-	wordList := "/usr/share/dict/words"
-	secretWord := getSecretWord(wordList)
-	if !isAllLetters(secretWord) {
-		t.Errorf("Should not get words with punctuations. Got %s", secretWord)
+	wordList, err := createDictFile([]string{"Lion's", "Elephant's", "monkey"})
+	defer os.Remove(wordList)
+	if err != nil {
+
+		t.Errorf("Couldn't create word list. Can't proceed with test : %v", err)
 	}
+	secretWord := getSecretWord(wordList)
+	if secretWord != "monkey" {
+		t.Errorf("Should get 'monkey' but Got %s", secretWord)
+	}
+	// wordList := "/usr/share/dict/words"
+	// secretWord := getSecretWord(wordList)
+	// if !isAllLetters(secretWord) {
+	// 	t.Errorf("Should not get words with punctuations. Got %s", secretWord)
+	// }
 }
 
 func TestSecretWordLength(t *testing.T) {
-	wordList := "/usr/share/dict/words"
-	secretWord := getSecretWord(wordList)
-	if len(secretWord) < 6 {
-		t.Errorf("Minimum word length is six. Got %s", secretWord)
+	wordList, err := createDictFile([]string{"lion", "pen", "monkey"})
+	defer os.Remove(wordList)
+	if err != nil {
+
+		t.Errorf("Couldn't create word list. Can't proceed with test : %v", err)
 	}
+	secretWord := getSecretWord(wordList)
+	if secretWord != "monkey" {
+		t.Errorf("Should get 'monkey' but Got %s", secretWord)
+	}
+	// wordList := "/usr/share/dict/words"
+	// secretWord := getSecretWord(wordList)
+	// if len(secretWord) < 6 {
+	// 	t.Errorf("Minimum word length is six. Got %s", secretWord)
+	// }
 }
